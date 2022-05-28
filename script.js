c551576ef0cb89977cc16bed06a5b3cc1e9f842b
mbudak <promaxcoq@gmail.com>
2022-05-28 01:10:24 -0400

    search terms are done

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 // your code
+const api_url = 'https://api.lyrics.ovh/suggest';
+const api_particular_song = 'https://api.lyrics.ovh/v1/${artist}/${songTitle}';
+
+var search_btn = document.forms['form'][1];
+var search_term = document.forms['form'][0];
+var result = document.getElementById('result');
+
+search_btn.addEventListener('click', searchSongs);
+
+const showData = (data) => {
+    result.innerHTML = `
+      <ul class="songs">
+        ${data.data
+          .map(
+            song => `<li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>`
+          )
+          .join('')}
+      </ul>
+    `;
+}
+
+const resultNotFound = () => {
+    result.innerHTML = `
+        <div class="centered not-found">Result not found</div>
+    `
+}
+
+
+async function searchSongs() {
+    event.preventDefault();
+    
+    fetch(`${api_url}/${search_term.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+        showData(data);
+    }).catch(() => {
+        resultNotFound();
+    });
+
+  }
+
