74aaf0897e0c1f685e412c36e54a9cd7e740386c
mbudak <promaxcoq@gmail.com>
2022-05-28 02:22:22 -0400

    lyrics are fetching now

--- script.js
+++ script.js
@@ -12,16 +12,32 @@ const showData = (data) => {
     result.innerHTML = `
       <ul class="songs">
         ${data.data
-          .map(
-            song => `<li>
+            .map(
+                song => `<li>
         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        <button onClick="showThisLyric(this)" class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
       </li>`
-          )
-          .join('')}
+            )
+            .join('')}
       </ul>
     `;
 }
+
+const showLyrics = (artist, songTitle, data) => {    
+    
+    result.innerHTML = `
+        <h1>Lyrics</h1>
+        <h2><b>${artist}</b> - ${songTitle}</h2>
+
+        
+        <pre class="lyrics">
+            ${data.lyrics}
+        </pre>
+        
+        
+    `
+}
+
 
 const resultNotFound = () => {
     result.innerHTML = `
@@ -32,14 +48,29 @@ const resultNotFound = () => {
 
 async function searchSongs() {
     event.preventDefault();
-    
-    fetch(`${api_url}/${search_term.value}`)
-    .then((response) => response.json())
-    .then((data) => {
-        showData(data);
-    }).catch(() => {
-        resultNotFound();
-    });
 
-  }
+    fetch(`${api_url}/${search_term.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            showData(data);
+        }).catch(() => {
+            resultNotFound();
+        });
+}
+
+async function showThisLyric(song) {
+    event.preventDefault();
+    const artist = song.getAttribute("data-artist");
+    const songTitle = song.getAttribute("data-songtitle");
+    result.innerHTML = `<h1>Loading...</h1>`;
+
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`)
+        .then((response) => response.json())
+        .then((data) => {            
+            showLyrics(artist, songTitle, data);
+        }).catch(() => {
+            result.innerHTML = "<h1>Lyrics not found</h1>"
+        });
+
+}
 
